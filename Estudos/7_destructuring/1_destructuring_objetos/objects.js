da34c03613b71769a88936c3748203c1c4248b41
Lays Almeida <laysnanda2012@gmail.com>
2025-06-20 16:07:36 -0300

    add destructuring classes

---
+++ Estudos/7_destructuring/1_destructuring_objetos/objects.js
@@ -0,0 +1,13 @@
+let car = {
+  rodas: 4,
+  portas: 4,
+  tetoSolar: true,
+  motor: "2.0",
+};
+
+const { rodas: cRodas, portas: cPortas, tetoSolar: cTetoSolar, motor: vMotor } = car;
+
+console.log(cRodas);
+console.log(cPortas);
+console.log(cTetoSolar);
+console.log(vMotor);
